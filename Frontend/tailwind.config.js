64654c5a524a0158f0ed80d2a479283a9918b60b
Amulyakantamneni <72382578+Amulyakantamneni@users.noreply.github.com>
2025-11-23 23:45:27 -0500

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./**/*.{js,ts,jsx,tsx,mdx}",
+    "!./node_modules",
+    "!./.next",
+    "!./out"
   ],
   theme: {
     extend: {},
